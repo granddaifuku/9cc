fe1c1202f384a859a69c850dcecf957751957748
granddaifuku <daifukuformac@gmail.com>
2021-08-02 11:12:48 +0900

    four arithmetic operatinos

--- 9cc.c
+++ 9cc.c
@@ -6,19 +6,40 @@
 #include <string.h>
 
 // Type of tokens
-typedef enum {
+  typedef enum {
   TK_RESERVED, // symbols
   TK_NUM, // numbers
   TK_EOF, // EOF
 } TokenKind;
 
+// Type of nodes in abstract syntax tree
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // Number
+} NodeKind;
+
 typedef struct Token Token;
+typedef struct Node Node;
+
+Node *expr();
+Node *mul();
+Node *primary();
 
 struct Token {
   TokenKind kind; // The type of token
   Token *next; // The next token
   int val; // The value of token if it is number
   char *str; // Token string
+};
+
+struct Node {
+  NodeKind kind; // The type of node
+  Node *lhs; // Left-hand side
+  Node *rhs; // Right-hand side
+  int val; // The value of node if it is number
 };
 
 // Current token
@@ -35,7 +56,7 @@ void error_at(char *loc, char *fmt, ...) {
 
   int pos = loc - user_input;
   fprintf(stderr, "%s\n", user_input);
-  fprintf(stderr, "%*s", pos, " "); // Output pos number of spaces.
+  fprintf(stderr, "%*s", pos, " "); // Output pos number of spaces
   fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
@@ -95,7 +116,7 @@ Token *tokenize() {
 	  p++;
 	  continue;
 	}
-	if (*p == '+' || *p == '-') {
+	if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
 	  cur = new_token(TK_RESERVED, cur, p++);
 	  continue;
 	}
@@ -113,6 +134,90 @@ Token *tokenize() {
   return head.next;
 }
 
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+	if (consume('+'))
+	  node = new_node(ND_ADD, node, mul());
+	else if (consume('-'))
+	  node = new_node(ND_SUB, node, mul());
+	else
+	  return node;
+  }
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+	if (consume('*'))
+	  node = new_node(ND_MUL, node, primary());
+	else if (consume('/'))
+	  node = new_node(ND_DIV, node, primary());
+	else
+	  return node;
+  }
+}
+
+Node *primary() {
+  if (consume('(')) {
+	Node *node = expr();
+	expect(')');
+	return node;
+  }
+
+  return new_node_num(expect_number());
+}
+
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+	printf("  push %d\n", node->val);
+	return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+  case ND_ADD:
+	printf("  add rax, rdi\n");
+	break;
+  case ND_SUB:
+	printf("  sub rax, rdi\n");
+	break;
+  case ND_MUL:
+	printf("  imul rax, rdi\n");
+	break;
+  case ND_DIV:
+	printf("  cqo\n");
+	printf("  idiv rdi\n");
+	break;
+  }
+
+  printf("  push rax\n");
+}
+
+
 int main(int argc, char **argv) {
   if (argc != 2) {
 	fprintf(stderr, "Invalid number of args\n");
@@ -123,26 +228,19 @@ int main(int argc, char **argv) {
   
   // Tokenize
   token = tokenize();
+  // Parse
+  Node *node = expr();
 
   // Export the first part of Assembly
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
-  // The first part of the expression should be a number.
-  // Check it and call mov.
-  printf("  mov rax, %d\n", expect_number());
+  // Generate code with recurisvely visiting the abstract syntax tree
+  gen(node);
 
-  while (!at_eof()) {
-	if (consume('+')) {
-	  printf("  add rax, %d\n", expect_number());
-	  continue;
-	}
-
-	expect('-');
-	printf("  sub rax, %d\n", expect_number());
-  }
-  
+  // The value of the entire expression remains the stack top
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
