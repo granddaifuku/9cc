be05521f509547e7478216ad56cbe6dc02dd69ec
granddaifuku <daifukuformac@gmail.com>
2021-08-03 14:01:48 +0900

    unary

--- 9cc.c
+++ 9cc.c
@@ -134,6 +134,7 @@ typedef struct Node Node;
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 struct Node {
@@ -159,6 +160,7 @@ Node *new_node_num(int val) {
   return node;
 }
 
+// expr = mul ("+" mul | "-" mul)*
 Node *expr() {
   Node *node = mul();
 
@@ -172,19 +174,32 @@ Node *expr() {
   }
 }
 
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
 	if (consume('*'))
-	  node = new_node(ND_MUL, node, primary());
+	  node = new_node(ND_MUL, node, unary());
 	else if (consume('/'))
-	  node = new_node(ND_DIV, node, primary());
+	  node = new_node(ND_DIV, node, unary());
 	else
 	  return node;
   }
 }
 
+// unary = ("+" | "-")? primary
+Node *unary() {
+  if (consume('+')) 
+	return primary();
+  
+  if (consume('-')) 
+	return new_node(ND_SUB, new_node_num(0), primary());
+  
+  return primary();
+}
+
+// primary = num | "(" expr ")"
 Node *primary() {
   if (consume('(')) {
 	Node *node = expr();
